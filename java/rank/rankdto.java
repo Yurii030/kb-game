ef6e4b1fffe78863a419077f83a941a75ebfbb6e
Ha-Eun-JUNG <87185626+Ha-Eun-JUNG@users.noreply.github.com>
2022-09-18 00:09:46 +0900

    Add files via upload

---
+++ java/rank/rankdto.java
@@ -0,0 +1,33 @@
+package rank;
+
+public class rankdto {
+	private String rid;
+	private int myrank;
+	private int totalrank;
+	private int myscore;
+	
+	public int getMyscore() {
+		return myscore;
+	}
+	public void setMyscore(int myscore) {
+		this.myscore = myscore;
+	}
+	public String getRid() {
+		return rid;
+	}
+	public void setRid(String rid) {
+		this.rid = rid;
+	}
+	public int getMyrank() {
+		return myrank;
+	}
+	public void setMyrank(int myrank) {
+		this.myrank = myrank;
+	}
+	public int getTotalrank() {
+		return totalrank;
+	}
+	public void setTotalrank(int totalrank) {
+		this.totalrank = totalrank;
+	}
+}
